7595e34a8501b00f233ec714bfa9a3eeb4dff44a
Ali Raza <aaliraza007@hotmail.com>
2024-08-15 15:11:16 +0500

    add some phyton method

---
+++ testpyton/code.py
@@ -0,0 +1,126 @@
+# print("Hello Python world!")
+# message:str ="i this is aliraza"
+# print(message.capitalize())
+
+# first_name:str = "ada"
+# last_name:str = "lovelace"
+# full_name:str = f"{first_name} {last_name}"
+# print(f"Hello, {full_name.title()}!")
+
+# name:str = "ada lovelace"
+# print(name.title())
+
+# bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+# print(bicycles)
+
+# mycar:list[str] = ["car1","car2","car3",1]
+# print(mycar)
+
+# bicycles: list[str] = ['trek', 'cannondale', 'redline', 'specialized']
+# print(bicycles[-1])
+
+# cars:list[str] = ['bmw', 'audi', 'toyota', 'subaru']
+# cars.sort()
+# print(cars)
+
+# motorcycles :list[str] = ['honda', 'yamaha', 'suzuki', 'ducati']
+# print(motorcycles)
+
+# too_expensive: str = 'ducati'
+# motorcycles.remove(too_expensive)
+# print(motorcycles)
+# print(f"A {too_expensive.title()} is too expensive for me.")
+
+# motorcycles:list[str] = []
+
+# motorcycles.append('honda')
+# motorcycles.append('yamaha')
+# motorcycles.append('suzuki')
+
+# print(motorcycles)
+
+# motorcycles: list[str] = ['honda', 'yamaha', 'suzuki']
+
+# motorcycles.insert(0, 'ducati')
+# print(motorcycles)
+
+# motorcycles:list[str] = ['honda', 'yamaha', 'suzuki']
+# print(motorcycles)
+
+# del motorcycles[0]
+
+# motorcycles.remove("suzuki")
+# print(motorcycles)
+
+# motorcycles:list[str] = ['honda', 'yamaha', 'suzuki']
+# print(motorcycles)
+
+# popped_motorcycle:str = motorcycles.pop()
+# print(motorcycles)
+# print(popped_motorcycle)
+
+# motorcycles:list[str] = ['honda', 'yamaha', 'suzuki']
+
+# last_owned:str = motorcycles.pop()
+# print(f"The last motorcycle I owned was a {last_owned.title()}.")
+# print(f"The last motorcycle I owned was a {motorcycles}.")
+
+# dimensions:tuple[int,int] = (200, 50)
+# print(dimensions[0])
+# print(dimensions[1])
+
+# dimensions:tuple[int,int,str] = (200, 50,"alir")
+# print(dimensions[0])
+# print(dimensions[2])
+# dimensions[0] = 250
+
+# dimensions:tuple[int,int] = (200, 50,1,2,3,4)
+# for dimension in dimensions:
+#     print(dimension)
+
+# dimensions:tuple[int,int] = (200, 50)
+# print("Original dimensions:")
+# for dimension in dimensions:
+#     print(dimension)
+
+# dimensions:tuple[int,int] = (400, 100)
+# print("\nModified dimensions:")
+# for dimension in dimensions:
+#     print(dimension)
+
+# even_numbers:list[int] = list(range(3, 30, 3))
+# print(even_numbers)
+
+# for value in range(1, 5):
+#     print(value)
+
+# for value in range(1, 6):
+#     print(value)
+
+# for value in range(1, 6):
+      
+#           print("Modified dimensions: {value}")
+
+
+# number:int = (range(1, 6))
+# print(number)
+
+# numbers:list[int] = list(range(1, 6))
+# print(numbers)
+
+
+# players = ['charles', 'martina', 'michael', 'florence', 'eli']
+# print(players[2:5])
+
+my_foods:list[str] = ['pizza', 'falafel', 'carrot cake']
+friend_foods:list[str] = my_foods[1:1]
+
+print("My favorite foods are:")
+print(my_foods)
+
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
+
+
+
+
